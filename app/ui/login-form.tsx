82f1922f965d50ddf1d72e7972bfd2592f0f168c
min <myun02302@gmail.com>
2023-11-15 22:56:56 +0900

    login-form modified

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -63,7 +63,7 @@ export default function LoginForm() {
         </div>
         <LoginButton />
         <div className="flex h-8 items-end space-x-1">
-          {state === 'CredentialSignin' && (
+          {state === 'CredentialsSignin' && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p aria-live="polite" className="text-sm text-red-500">
