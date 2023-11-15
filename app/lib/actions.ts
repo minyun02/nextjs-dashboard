bd1e275a2213c433100682a57b3d7e56670e0836
min <myun02302@gmail.com>
2023-11-15 21:25:20 +0900

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,9 +18,19 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function deleteInvoice(id: string) {
-  await sql`
-    DELETE FROM invoices WHERE id = ${id}
-  `;
+  throw new Error('delete invoice failed')
+
+  try {
+    await sql`
+      DELETE FROM invoices WHERE id = ${id}
+    `;  
+    return { measage: 'Deleted Invoice. '};
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.'
+    };
+  }
+  
   revalidatePath('/dashboard/invoices');
 }
 
@@ -33,12 +43,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;  
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.'
+    }
+  }
+  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -53,10 +69,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;      
+    } catch (error) {
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
